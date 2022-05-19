35720eaceb67639712b766071eabcdcb0f446f32
Arek Tarnawski <86805618+arektarnawski@users.noreply.github.com>
2022-05-19 15:31:56 +0200

    Update streamlit_app.py
    Added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥑 Omega 3 & Blueberry Oatmeal')
@@ -13,4 +16,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.dataframe(my_fruit_list)
+
